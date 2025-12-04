1d70c6d0db0d0f838791e0dfcf4999141f11ef6c
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-04 16:42:51 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <center>
+        <h1>Hello and welcome to our app</h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;
