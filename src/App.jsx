f7c61c6cdc7bc7ebe829a3cfa0bb295ee588a7bd
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-05 11:32:15 +0600

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
 
 function App() {
   return (
     <div>
-      <center>
-        <h1>Hello and welcome to our app</h1>
-      </center>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/login" element={<CaptainSignup />} />
+      </Routes>
     </div>
   );
 }
